6e7929b290db2c5e9899dae1c0a07134c3b77997
LautaLope <bas.gdmc@gmail.com>
2024-09-24 17:53:05 -0300

    backEnd terminado

--- backEnd/model/tokenDAO.php
+++ backEnd/model/tokenDAO.php
@@ -1,12 +1,14 @@
 <?php
 
-
-function createToken($userName, $password)
+class token
 {
-    if ($userName == "Pablo" && $password == "Admin") {
-        $token = base64_encode(json_encode(['userName' => $userName, 'exp' => time() + 30]));
-        $signature = hash_hmac('sha256', $token, $password);
+    function create($userName, $password)
+    {
+        if ($userName == "Pablo" && $password == "Admin") {
+            $token = base64_encode(json_encode(['userName' => $userName, 'exp' => time() + 30]));
+            $signature = hash_hmac('sha256', $token, $password);
 
-        return $token . '.' . $signature;
+            return $token . '.' . $signature;
+        }
     }
 }
